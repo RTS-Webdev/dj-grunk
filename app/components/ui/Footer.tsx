6a66875bce6f1520349e079dde9e27bf08598182
Mikkel <mikkelelvers@outlook.com>
2024-11-04 14:56:52 +0100

    Update Footer.tsx

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className="p-8 text-center text-sm text-gray-600 bottom-0 right-0 left-0 fixed">
+        <footer className="text-gray-600">
             <p>&copy; {new Date().getFullYear()}, DJ Grunk. Alle rettigheder forbeholdes.</p>
         </footer>
     )
